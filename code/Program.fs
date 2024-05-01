867e9a94926ce09420d325c190e8e347a87f0591
Jocelyn Bliven <jmb13@williams.edu>
2024-05-01 13:26:37 -0400

    added usage

--- code/Program.fs
+++ code/Program.fs
@@ -5,15 +5,19 @@ open Parser
 
 [<EntryPoint>]
 let main args =
-    let file = args[0]
-    let text = IO.File.ReadAllText file
-    //let text = args[0]
-    match parse text with
-    | Some ast ->
-        //printfn "%A" ast
-        let svg = eval ast
-        printfn "%s" svg
-        0
-    | None ->
-        printfn "Invalid program."
+    if args.Length < 1 then 
+        printfn "usage is dotnet run testcode.txt > test.svg"
         1
+    else 
+        let file = args[0]
+        let text = IO.File.ReadAllText file
+        //let text = args[0]
+        match parse text with
+        | Some ast ->
+            //printfn "%A" ast
+            let svg = eval ast
+            printfn "%s" svg
+            0
+        | None ->
+            printfn "Invalid program."
+            1
