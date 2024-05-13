8a4d9ccdcd8c193d5f0189cdeaa942e02279b84a
Skylar Yarter <soy1@williams.edu>
2024-05-13 15:30:49 -0400

    merge??

--- code/lang/Evaluator.fs
+++ code/lang/Evaluator.fs
@@ -17,34 +17,34 @@ let evalplayer p t force =
     | Offense(a, False), Manual ->
         let x = a.x |> string
         let y = a.y |> string
-        " cx=\"" + x + "\" cy=\"" + y + "\" fill=\"black\" />"
+        "  <circle r=\"7\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"black\" />"
     | Offense(a, True), Manual ->
         let x = a.x |> string
         let y = a.y |> string
         let x1 = a.x+4 |> string
         let y1 = a.y+4 |> string
-        " cx=\"" + x + "\" cy=\"" + y + "\" fill=\"black\" />" +
+        "  <circle r=\"7\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"black\" />" +
         "  <circle r=\"3\" cx=\"" + x1 + "\" cy=\"" + y1 + "\" fill=\"white\" />" +
         "<text x=\"20\" y=\"480\" fill=\"black\"> Recommended Play: " + (evalPlay force a) + "</text>"
     | Defense(a), _ -> 
         let x = a.x |> string
         let y = a.y |> string
-        " cx=\"" + x + "\" cy=\"" + y + "\""
+        "  <circle r=\"7\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"red\" />"
     | Offense(a, _), Automatic -> // automatic person defense (not really offense)
-        let x = a.x |> string
-        let y = a.y |> string
-        " cx=\"" + x + "\" cy=\"" + y + "\""
+        let x = (a.x+5) |> string
+        let y = (a.y+5) |> string
+        "  <circle r=\"7\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"red\" />"
 
 let rec evalplayers team flag force = 
     match team with 
     | Offensive(ol) ->
         match ol with 
         | [] -> ""
-        | o::ol -> "  <circle r=\"7\""+ (evalplayer o flag force) + " fill=\"black\" />"+ (evalplayers(Offensive(ol)) flag force)
+        | o::ol -> (evalplayer o flag force) + (evalplayers(Offensive(ol)) flag force)
     | Defensive(dl) ->
         match dl with 
         | [] -> ""
-        | d::dl -> "  <circle r=\"7\""+ (evalplayer d flag force) + " fill=\"red\" />" + (evalplayers(Defensive(dl)) flag force) 
+        | d::dl -> (evalplayer d flag force) + (evalplayers(Defensive(dl)) flag force) 
 
 let eval (field: Field) : string =
     let (teams, flag, force) = field
